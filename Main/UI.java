c10f1f31ee1d1ec1c9c341889bea1c4b3e4def79
Mishidev-2 <shimizumarkrey2007@gmail.com>
2026-05-26 19:10:16 +0800

    Edit #1 - Font

--- Main/UI.java
+++ Main/UI.java
@@ -1,7 +1,6 @@
 package Main;
 
 import java.awt.*;
-import javax.swing.*;
 
 public class UI {
 
@@ -82,7 +81,7 @@ public class UI {
         g2.fillRoundRect(pad, contentY, leftW, contentH, 25, 25);
 
         g2.setColor(Color.WHITE);
-        g2.setFont(new Font("Impact", Font.PLAIN, 16));
+        g2.setFont(new Font("Inter", Font.PLAIN, 16));
         g2.drawString("LIBRARY", pad + 55, contentY + 32);
 
         int rightX = pad + leftW + gap;
@@ -94,7 +93,7 @@ public class UI {
         g2.setColor(Color.WHITE);
         g2.drawString("PLAYLISTS", rightX + 50, contentY + 32);
         g2.setColor(Color.GRAY);
-        g2.setFont(new Font("Impact", Font.BOLD, 14));
+        g2.setFont(new Font("Inter", Font.PLAIN, 14));
         g2.drawString("None", rightX + (rightW / 2) - 18, contentY + (subH / 2) + 5);
 
         int playerY = contentY + subH + gap;
@@ -102,7 +101,7 @@ public class UI {
         g2.fillRoundRect(rightX, playerY, rightW, subH, 25, 25);
 
         g2.setColor(Color.WHITE);
-        g2.setFont(new Font("Impact", Font.PLAIN, 16));
+        g2.setFont(new Font("Inter", Font.PLAIN, 16));
         g2.drawString("PLAYER", rightX + 20, playerY + 32);
     }
 }
