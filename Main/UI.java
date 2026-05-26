0121e542b6c419a8512481ca210a0d694f6dbe64
SiZUruuU <savagefeeble@gmail.com>
2026-05-26 18:56:08 +0800

    page ui created

--- Main/UI.java
+++ Main/UI.java
@@ -15,6 +15,7 @@ public class UI {
 
     public void draw(Graphics2D g2){
         panel.setBackground(Color.decode("#37008f"));
+        drawPage(g2);
         if(exit){ drawExitInquiry(g2);}
 
     }
@@ -50,5 +51,58 @@ public class UI {
 
         g2.setColor(Color.BLACK);
         g2.drawString("No", 355, 370);
+    }
+
+    public void drawPage(Graphics2D g2){
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        int w = panel.getWidth();
+        int h = panel.getHeight();
+
+        g2.setColor(Color.decode("#051261"));
+        g2.fillRect(0, 0, w, h);
+
+        int pad = 25;
+        int gap = 15;
+
+        int totalUsableWidth = w - (pad * 2) - gap;
+        int leftW = (int) (totalUsableWidth * 0.55); 
+        int rightW = totalUsableWidth - leftW;
+
+        int searchBarY = pad + 10;
+        int searchBarH = 32;
+        int contentY = searchBarY + searchBarH + 20;
+        int contentH = h - contentY - pad;
+
+        g2.setColor(Color.decode("#0044b3"));
+        int searchBarW = (int) (leftW * 1.0); 
+        g2.fillRoundRect(pad, searchBarY, searchBarW, searchBarH, 15, 15);
+
+        g2.setColor(Color.decode("#010626"));
+        g2.fillRoundRect(pad, contentY, leftW, contentH, 25, 25);
+
+        g2.setColor(Color.WHITE);
+        g2.setFont(new Font("Impact", Font.PLAIN, 16));
+        g2.drawString("LIBRARY", pad + 55, contentY + 32);
+
+        int rightX = pad + leftW + gap;
+        int subH = (contentH - gap) / 2; 
+
+        g2.setColor(Color.decode("#010626"));
+        g2.fillRoundRect(rightX, contentY, rightW, subH, 25, 25);
+
+        g2.setColor(Color.WHITE);
+        g2.drawString("PLAYLISTS", rightX + 50, contentY + 32);
+        g2.setColor(Color.GRAY);
+        g2.setFont(new Font("Impact", Font.BOLD, 14));
+        g2.drawString("None", rightX + (rightW / 2) - 18, contentY + (subH / 2) + 5);
+
+        int playerY = contentY + subH + gap;
+        g2.setColor(Color.decode("#010626"));
+        g2.fillRoundRect(rightX, playerY, rightW, subH, 25, 25);
+
+        g2.setColor(Color.WHITE);
+        g2.setFont(new Font("Impact", Font.PLAIN, 16));
+        g2.drawString("PLAYER", rightX + 20, playerY + 32);
     }
 }
